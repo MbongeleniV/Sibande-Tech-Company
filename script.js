1c5cb30be40fce57d28ff7a86df48412261da028
MbongeleniV <draggyemm@gmail.com>
2026-04-05 21:32:36 +0200

    Added Animations

--- script.js
+++ script.js
@@ -6,4 +6,55 @@ document.addEventListener("DOMContentLoaded", function () {
         navbar.classList.toggle("show");
         hamburger.textContent = navbar.classList.contains("show") ? "✖" : "☰";
     });
+
 });
+
+const elements = document.querySelectorAll('.image4');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
+
+////////////////////////////
+const elements2 = document.querySelectorAll('.image');
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        }
+    });
+});
+
+elements2.forEach(el => observer2.observe(el));
+/////////////////////////
+
+const elements3 = document.querySelectorAll('.occupationImage');
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show3');
+        }
+    });
+});
+elements3.forEach(el => observer3.observe(el));
+////////////////////////////
+
+
+const elements4 = document.querySelectorAll('form');
+
+const observer4 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show4');
+        }
+    });
+});
+elements4.forEach(el => observer4.observe(el));
